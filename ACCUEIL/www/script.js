85f6e09118b6d5b990471f5bb5543fe88bfcee46
KevinRoelandt <96341394+kevin1211@users.noreply.github.com>
2024-06-01 01:01:55 +0200

    version non fini

--- ACCUEIL/www/script.js
+++ ACCUEIL/www/script.js
@@ -1,5 +1,5 @@
 document.getElementById('redirectButton1').addEventListener('click', function() {
-    window.location.href = '../../MOI/www/index.html';
+    window.location.href = '../../INFORMATIONS/www/index.html';
 });
 document.getElementById('redirectButton2').addEventListener('click', function() {
     window.location.href = '../../COCOVOIT/www/index.html';
