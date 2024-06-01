e9ecc11dcc88e337f3736799b89de84a1a0a04cd
KevinRoelandt <96341394+kevin1211@users.noreply.github.com>
2024-06-01 19:55:32 +0200

    modif liens téléchargement rendue fonctionnelle

--- INFORMATIONS/www/script.js
+++ INFORMATIONS/www/script.js
@@ -1,7 +1,8 @@
 function downloadFile(fileName) {
+    const filePath = '../doc/' + fileName;
     const link = document.createElement('a');
-    link.href = fileName;
-    link.download = fileName;
+    link.href = filePath;
+    link.setAttribute('download', fileName);
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
