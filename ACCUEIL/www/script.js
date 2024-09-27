80eb7c2ca0c360ae92285abf7a145d5fc3b0f90c
KevinRoelandt <96341394+kevin1211@users.noreply.github.com>
2024-09-27 21:08:39 +0200

    relier permis a la page accueil

--- ACCUEIL/www/script.js
+++ ACCUEIL/www/script.js
@@ -4,6 +4,6 @@ document.getElementById('redirectButton1').addEventListener('click', function() 
 document.getElementById('redirectButton2').addEventListener('click', function() {
     window.location.href = '../../COCOVOIT/www/index.html';
 });
-document.getElementById('redirectButton2').addEventListener('click', function () {
+document.getElementById('redirectButton3').addEventListener('click', function () {
     window.location.href = '../../PERMIS/www/index.html';
 });
